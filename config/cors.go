da16daa6a0f51f71ec198ed016cfeb59bbd1e0a1
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2026-02-03 20:47:25 +0700

    fix(cors): inisialisasi vercel app kedalam allow origin

--- config/cors.go
+++ config/cors.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2/middleware/cors"
 // setup cors
 func SetupCORS() cors.Config {
 	return cors.Config{
-		AllowOrigins: "http://localhost:5173,https://ulasin-frontend.vercel.app",
+		AllowOrigins: "http://localhost:5173,https://ulasin-frontend.vercel.app,https://ulasin-frontend-bn4z8eyc7-muhamad-ridwans-projects-fddf9a10.vercel.app",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}
