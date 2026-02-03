5cd94e78dc45d0608fb522810a3360bb8d06efab
muhamadridwan1414 <mr1401.ridwan@gmail.com>
2026-02-03 20:08:45 +0700

    feat(cors): inisialisasi vercel kedalam allow origins

--- config/cors.go
+++ config/cors.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2/middleware/cors"
 // setup cors
 func SetupCORS() cors.Config {
 	return cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "http://localhost:5173,https://ulasin-frontend.vercel.app",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}
